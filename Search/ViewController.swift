a2537d4be391ad7601eef239d26988fea916212e
森部高昌 <takamasamoribe@gmail.com>
2021-10-11 21:23:26 +0900

    1011  1   最初　検索で日本語変換にならない

--- Search/ViewController.swift
+++ Search/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var textField: UITextField!
+    
+    private var searchCompleter = MKLocalSearchCompleter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchCompleter.delegate = self
     }
-
-
+    
+    @IBAction private func textFieldEditingChanged(_ sender: Any) {
+        // あとで
+        searchCompleter.queryFragment = textField.text!
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchCompleter.results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // あとで
+        let completion = searchCompleter.results[indexPath.row]
+                cell.textLabel?.text = completion.title
+                cell.detailTextLabel?.text = completion.subtitle
+        
+        return cell
+    }
+}
+
+
+extension ViewController: MKLocalSearchCompleterDelegate {
+    
+    // 正常に検索結果が更新されたとき
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        tableView.reloadData()
+    }
+    
+    // 検索が失敗したとき
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        // エラー処理
+    }
+}
