ea7074736585fb80df3e7fe4db439ef0a61a1f21
森部高昌 <takamasamoribe@gmail.com>
2021-11-01 21:11:32 +0900

    1101  1    Json関係のコードを取り除いた

--- Search/ViewController.swift
+++ Search/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 
 
-class ViewController: UIViewController,UISearchBarDelegate {
+class ViewController: UIViewController,UISearchBarDelegate,XMLParserDelegate {
     
     @IBOutlet weak var searchText: UISearchBar!
     @IBOutlet private weak var tableView: UITableView!
@@ -24,7 +24,7 @@ class ViewController: UIViewController,UISearchBarDelegate {
             searchPlace(keyword: searchWord)
         }
     }
-  
+    
     //searchPlace メソッド
     // 第一引数：keyword 検索したい語句
     func searchPlace(keyword:String) {
@@ -36,6 +36,17 @@ class ViewController: UIViewController,UISearchBarDelegate {
         guard let req_url = URL(string: "https://geocode.csis.u-tokyo.ac.jp/cgi-bin/simple_geocode.cgi?addr=\(keyword_encode)") else {
             return
         }
+        // リクエストに必要な情報を生成する
+        let req = URLRequest(url: req_url)
+        
+        let task = URLSession.shared.dataTask(with: req, completionHandler: {(data,response,error) in
+        let parser:XMLParser? = XMLParser(data:data!)
+        parser!.delegate = self
+        parser!.parse()
+        })
+        
+        // ダウンロード開始
+        task.resume()
         print(req_url)//入力されていたら、urlを表示する
     }
     
@@ -47,58 +58,6 @@ class ViewController: UIViewController,UISearchBarDelegate {
         
         searchText.delegate = self
         searchText.placeholder = "検索する地名を入力"
-                
     }
     
-    
-    
-// --------------------------------------
-//        tableView.delegate = self
-//        tableView.dataSource = self
-//        searchCompleter.delegate = self
-//
-//        // 東京駅を中心にして検索する
-//        let tokyoStation = CLLocationCoordinate2DMake(35.6811673, 139.7670516) // 東京駅
-//        let span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001) // ここは適当な値です
-//        let region = MKCoordinateRegion(center: tokyoStation, span: span)
-//        searchCompleter.region = region
-//
-//        searchCompleter.resultTypes = .address //地図上の位置のみ検索する
-////        searchCompleter.resultTypes = .pointOfInterest //
-////        searchCompleter.resultTypes = .query //
-// --------------------------------------
-        
 }
-
-
-//--------------------------------
-//extension ViewController: UITableViewDelegate, UITableViewDataSource {
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return searchCompleter.results.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-//        //
-//        let completion = searchCompleter.results[indexPath.row]
-//                cell.textLabel?.text = completion.title
-//                cell.detailTextLabel?.text = completion.subtitle
-//
-//        return cell
-//    }
-//}
-//
-//
-//extension ViewController: MKLocalSearchCompleterDelegate {
-//
-//    // 正常に検索結果が更新されたとき
-//    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
-//        tableView.reloadData()
-//    }
-//
-//    // 検索が失敗したとき
-//    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
-//        // エラー処理
-//    }
-//}
